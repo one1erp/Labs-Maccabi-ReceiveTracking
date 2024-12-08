d78ce956e637c69b6aed575adc2a8c2f33e84df3
oneprojectsa <ashim@one1.co.il>
2024-12-08 11:04:05 +0200

    Handle Virtual Entities

---
+++ ReceiveTracking/TrackEntities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReceiveTracking
+{
+    class TrackEntities
+    {
+        public string id { get; set; }
+        public string tableName { get; set; }
+
+        public TrackEntities(string id, string tableName)
+        {
+            this.id = id;
+            this.tableName = tableName;
+        }
+    }
+}
